dd3883f3f47fcc9e705150cac41d5ceb5bf1bbf7
Theo Sun <theo.sun.github@outlook.com>
2022-05-20 17:40:37 +0800

    chore: update base64 related impl

--- src/base64.ts
+++ src/base64.ts
@@ -1,45 +1,13 @@
-// @ts-nocheck
-// ref https://gist.github.com/aleiphoenix/5472119
-const keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
 /**
  * encode string to base64
  *
  * @param input
  */
-const encode = function(input = ""): string {
-  input = input;
-  let output = "";
-  let chr1, chr2, chr3 = "";
-  let enc1, enc2, enc3, enc4 = "";
-  let i = 0;
-
-  do {
-    chr1 = input.charCodeAt(i++);
-    chr2 = input.charCodeAt(i++);
-    chr3 = input.charCodeAt(i++);
-
-    enc1 = chr1 >> 2;
-    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-    enc4 = chr3 & 63;
-
-    if (isNaN(chr2)) {
-      enc3 = enc4 = 64;
-    } else if (isNaN(chr3)) {
-      enc4 = 64;
-    }
-
-    output = output +
-      keyStr.charAt(enc1) +
-      keyStr.charAt(enc2) +
-      keyStr.charAt(enc3) +
-      keyStr.charAt(enc4);
-    chr1 = chr2 = chr3 = "";
-    enc1 = enc2 = enc3 = enc4 = "";
-  } while (i < input.length);
-
-  return output;
+const encode = function (input = ""): string {
+  if (globalThis.Buffer !== undefined) {
+    return globalThis.Buffer.from(input, "utf-8").toString("base64");
+  }
+  return btoa(input);
 };
 
 /**
@@ -47,46 +15,11 @@ const encode = function(input = ""): string {
  *
  * @param input base64 string
  */
-const decode = function(input: string): string {
-  let output = "";
-  let chr1, chr2, chr3 = "";
-  let enc1, enc2, enc3, enc4 = "";
-  let i = 0;
-
-  // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-  const base64test = /[^A-Za-z0-9\+\/\=]/g;
-  if (base64test.exec(input)) {
-    throw new Error("There were invalid base64 characters in the input text.\n" +
-      "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-      "Expect errors in decoding.");
+const decode = function (input: string): string {
+  if (globalThis.Buffer !== undefined) {
+    return globalThis.Buffer.from(input, "base64").toString("utf-8");
   }
-  input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-  do {
-    enc1 = keyStr.indexOf(input.charAt(i++));
-    enc2 = keyStr.indexOf(input.charAt(i++));
-    enc3 = keyStr.indexOf(input.charAt(i++));
-    enc4 = keyStr.indexOf(input.charAt(i++));
-
-    chr1 = (enc1 << 2) | (enc2 >> 4);
-    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-    chr3 = ((enc3 & 3) << 6) | enc4;
-
-    output = output + String.fromCharCode(chr1);
-
-    if (enc3 != 64) {
-      output = output + String.fromCharCode(chr2);
-    }
-    if (enc4 != 64) {
-      output = output + String.fromCharCode(chr3);
-    }
-
-    chr1 = chr2 = chr3 = "";
-    enc1 = enc2 = enc3 = enc4 = "";
-
-  } while (i < input.length);
-
-  return output;
+  return atob(input);
 };
 
 
