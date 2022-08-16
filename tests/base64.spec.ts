475e93c8b51d925201f6b672036ae74270913715
Theo Sun <theo.sun@outlook.com>
2022-08-16 18:22:17 +0800

    fix: base64 utf8

---
+++ tests/base64.spec.ts
@@ -0,0 +1,17 @@
+import { encode, encodeJs, decode, decodeJs } from "../src/base64";
+
+
+describe('base64 Test Suite', () => {
+
+  it('should be same', () => {
+
+    for (let idx = 0; idx < 10; idx++) {
+      // generate a random string
+      const input = Math.random().toString(36);
+      expect(encode(input)).toBe(encodeJs(input));
+      expect(decode(encode(input))).toBe(decodeJs(encodeJs(input)));
+    }
+
+  });
+
+});
